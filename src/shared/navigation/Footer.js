175d3629c9854d69836c05133ce4ae18a4be9529
Dzenis Gudzevic <110186379+Dzenoo@users.noreply.github.com>
2023-03-06 22:14:02 +0100

    Styling Footer

--- src/shared/navigation/Footer.js
+++ src/shared/navigation/Footer.js
@@ -1,45 +1,46 @@
+import { Grid, List, ListItem, Typography } from "@mui/material";
 import React from "react";
 
 import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div className="footer_wrapper">
-      <div className="footer_grid">
-        <div className="footer_column1">
-          <h1>About Us</h1>
-          <p>
+    <>
+      <Grid container spacing={12} padding={10}>
+        <Grid item xs={4}>
+          <Typography variant="h4">About Us</Typography>
+          <Typography variant="p">
             Codinghero is your go-to source for programming tutorials and blogs.
             Our mission is to help you build your skills and stay on top of the
             latest trends in programming.
-          </p>
-        </div>
-        <div className="footer_column2">
-          <h1>Contact Us</h1>
-          <p>
+          </Typography>
+        </Grid>
+        <Grid item xs={4}>
+          <Typography variant="h4">Contact Us</Typography>
+          <Typography variant="p">
             If you have any questions or feedback, please don't hesitate to get
             in touch with us. You can reach us by emailing codinghero@gmail.com
             or by following us on social media.
-          </p>
-        </div>
-        <div className="footer_column3">
-          <h1>Stay Connected</h1>
-          <p>
+          </Typography>
+        </Grid>
+        <Grid item xs={4}>
+          <Typography variant="h4">Stay Connected</Typography>
+          <Typography variant="p">
             Follow us on social media to stay up-to-date with the latest
             tutorials and blog posts from codinghero. You can find us on:
-          </p>
-          <ul>
-            <li>Instagram</li>
-            <li>Discord</li>
-            <li>Facebook</li>
-          </ul>
-        </div>
-      </div>
+          </Typography>
+          <List>
+            <ListItem>Instagram</ListItem>
+            <ListItem>Discord</ListItem>
+            <ListItem>Facebook</ListItem>
+          </List>
+        </Grid>
+      </Grid>
 
-      <p style={{ textAlign: "center", paddingBottom: "40px" }}>
+      <Typography variant="p" textAlign="center">
         Copyright © 2023 codinghero. All rights reserved.
-      </p>
-    </div>
+      </Typography>
+    </>
   );
 };
 
