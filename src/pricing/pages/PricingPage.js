b587a9a667856f1f9ecb5f5aaa982e217c336298
Dzenis Gudzevic <110186379+Dzenoo@users.noreply.github.com>
2023-03-09 22:15:52 +0100

    Adding accordions to plans

--- src/pricing/pages/PricingPage.js
+++ src/pricing/pages/PricingPage.js
@@ -1,7 +1,117 @@
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Button,
+  Card,
+  CardContent,
+  Grid,
+  List,
+  ListItem,
+  Typography,
+} from "@mui/material";
+import { Container } from "@mui/system";
+import { PLANSE } from "../../shared/data/data";
+import { AccordionPlans } from "../../shared/data/data";
 import React from "react";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+
+const plan = [{}];
 
 const PricingPage = () => {
-  return <div className="pricingPage">PricingPage</div>;
+  return (
+    <>
+      <div className="pricingPage">
+        <Container
+          maxWidth="md"
+          sx={{
+            padding: "40px 0",
+            textAlign: "center",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: "2em",
+          }}
+        >
+          <Typography variant="p" color="lightgrey" fontWeight="bold">
+            PRICING
+          </Typography>
+          <Typography variant="h2" fontWeight="bold" color="white">
+            Get Started Now,
+            <br /> Pick a Plan Later
+          </Typography>
+          <Typography variant="p" color="white">
+            Choose the plan that best fits your needs and budget. Our plans
+            offer a range of features and services to help you achieve your
+            goals, whether you're an individual, small business, or enterprise.{" "}
+          </Typography>
+          <Button sx={{ backgroundColor: "#fcbf49", color: "#fff" }}>
+            No credit card required
+          </Button>
+        </Container>
+      </div>
+
+      <Grid container spacing={4} justifyContent="center" padding={4}>
+        {PLANSE.map((plan) => (
+          <Grid item xl={3} lg={3} md={5} xs={12} key={plan.id}>
+            <Card
+              sx={{
+                height: "100%",
+                textAlign: "center",
+              }}
+            >
+              <CardContent>
+                <Typography variant="h3" sx={{ margin: "20px 0 20px 0" }}>
+                  {plan.type}
+                </Typography>
+                <Typography variant="p">{plan.desc}</Typography>
+                <Button
+                  variant="contained"
+                  fullWidth
+                  size="large"
+                  sx={{ margin: "20px 0 40px 0" }}
+                >
+                  Get Started
+                </Button>
+                <Typography variant="h3" fontWeight="bold">
+                  {plan.price}
+                </Typography>
+                <List>
+                  {plan.features.map((f) => (
+                    <ListItem key={f}>
+                      <CheckCircleIcon
+                        style={{ fill: "#fcbf49", marginRight: "10px" }}
+                      />
+                      {f}
+                    </ListItem>
+                  ))}
+                </List>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+
+      <Container maxWidth="md" sx={{ padding: "40px 0" }}>
+        {AccordionPlans.map((ac) => (
+          <Accordion security="str" key={ac}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>{ac.title}</Typography>
+            </AccordionSummary>
+
+            <AccordionDetails>
+              <Typography>{ac.details}</Typography>
+            </AccordionDetails>
+          </Accordion>
+        ))}
+      </Container>
+    </>
+  );
 };
 
 export default PricingPage;
---
+++ src/shared/data/data.js
@@ -0,0 +1,114 @@
+export const PLANSE = [
+  {
+    id: "1",
+    type: "Basic Plan",
+    desc: "The Basic plan provides everything you need to get started. With 24/7 support and access to all core features",
+    price: "$10/mo",
+    features: [
+      "Access to basic programming courses",
+      "Access to practice challenges",
+      "Limited access to community forum",
+      "Email support",
+      "Access to basic features",
+    ],
+  },
+  {
+    id: "2",
+    type: "Pro Plan",
+    desc: "The Pro plan is perfect for businesses that need advanced features and priority support. ",
+    price: "$30/mo",
+    features: [
+      "All Basic Plan features",
+      "Access to intermediate programming courses",
+      "Access to advanced practice challenges",
+      "Priority email support",
+      "Access to advanced features",
+      "Access to a private community forum",
+      "Personalized feedback on submitted challenges",
+    ],
+  },
+  {
+    id: "3",
+    type: "Enterprise Plan",
+    desc: "The Enterprise plan is the ultimate package for large organizations that need a custom solution.",
+    price: "$60/mo",
+    features: [
+      "All Pro Plan features",
+      "Access to expert programming courses",
+      "Access to custom practice challenges",
+      "24/7 phone and email support",
+      "Customizable learning path",
+      "Access to exclusive content",
+      "Dedicated mentorship from experienced developers",
+      "Job placement assistance",
+      "Unlimited access to all features and resources",
+    ],
+  },
+];
+
+export const AccordionsBasic = [
+  {
+    title: "What programming languages are available on the platform?",
+    details:
+      "Our platform offers a wide range of programming languages to learn, including JavaScript, and other tools, React, Html, Css, and more.",
+  },
+  {
+    title:
+      "Is the platform suitable for beginners or only experienced programmers?",
+    details:
+      "Our platform is designed to cater to both beginners and experienced programmers. We offer courses for all skill levels and provide a variety of resources to support learners at every stage of their learning journey.",
+  },
+  {
+    title:
+      "Are there any limitations on the number of projects I can create on the platform?",
+    details:
+      "There are no limitations on the number of projects you can create on our platform. We want to encourage you to practice and experiment as much as you can!",
+  },
+  {
+    title: "Can I access the platform on multiple devices?",
+    details:
+      "Yes, you can access our platform on multiple devices, including desktops, laptops, tablets, and smartphones. You can also seamlessly switch between devices while working on a project.",
+  },
+  {
+    title: "How frequently are new courses and content added to the platform?",
+    details:
+      "We are constantly adding new courses and content to our platform to keep up with the latest trends and technologies in the programming world. We strive to provide the most up-to-date and relevant resources for our users.",
+  },
+  {
+    title: "What payment methods are accepted on the platform?",
+    details: "We accept  payment methods on our platform, with PayPal.",
+  },
+];
+
+export const AccordionPlans = [
+  {
+    title: "What payment methods do you accept?",
+    details:
+      "We accept payments through major credit cards, PayPal, and bank transfers. You can choose the payment method that suits you best.",
+  },
+  {
+    title: "Is there a free trial available for the plans?",
+    details:
+      "Yes, we offer a free trial period for all our plans. You can sign up for a trial account and test our platform before deciding to purchase a plan.",
+  },
+  {
+    title: "How long is the subscription period?",
+    details:
+      "The subscription period varies depending on the plan you choose. Our plans are available on a monthly or annual basis, and you can choose the option that fits your needs.",
+  },
+  {
+    title: "What happens to my data if I cancel my subscription?",
+    details:
+      "If you cancel your subscription, your data will be saved for a certain period of time. After that, your data will be deleted permanently. However, you can export your data at any time before canceling your subscription.",
+  },
+  {
+    title: "Are there any hidden fees?",
+    details:
+      " No, there are no hidden fees. The price you see on our plans page is the price you will pay. We are transparent about our pricing and there are no surprises.",
+  },
+  {
+    title: "Can I change my plan at any time?",
+    details:
+      "Yes, you can change your plan at any time. You can upgrade or downgrade your plan from your account dashboard with just a few clicks.",
+  },
+];
