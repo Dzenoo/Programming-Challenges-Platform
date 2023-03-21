65a37ee825b677035a334d9f20825d402d6999d9
Dzenis Gudzevic <110186379+Dzenoo@users.noreply.github.com>
2023-03-21 21:56:55 +0100

    Update BlogInput.js

--- src/blog/components/BlogInput.js
+++ src/blog/components/BlogInput.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { TextField } from "@mui/material";
 import "../css/blog.css";
+
 const TutorialInput = ({ setInputValue, searchTutorials, searchMsg }) => {
   return (
     <form onSubmit={searchTutorials}>
